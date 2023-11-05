74c9124c4036a09b02ee117071e42d5ecd6ac880
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-11-05 15:28:06 +0300

    Рефакторинг

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,7 @@
 from typing import List
-
 from pydantic import EmailStr
 from sqlalchemy import Integer, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
 from app.models.base_class import Base
 
 
