1ac5dd29b7f05658f4579bc6dceb2e4b00802503
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-11-06 16:02:15 +0300

    Изменил структуру проекта (маршрутизаторы)

--- app/api/user.py
+++ app/api/user.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter
-from app.api.v1 import route_user
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import get_async_session
+from app.repository.user import create_new_user
+from app.schemas.user import UserCreate, GetUser
 
 
-api_router = APIRouter()
-api_router.include_router(route_user.router, prefix="", tags=["users"])
+api_router = APIRouter(prefix="", tags=["users"])
+
+
+@api_router.post("/", response_model=GetUser, status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserCreate, db: AsyncSession = Depends(get_async_session)):
+    user = await create_new_user(user=user, db=db)
+    return user
--- app/main.py
+++ app/main.py
@@ -1,29 +1,17 @@
+import uvicorn
 from fastapi import FastAPI
-from app.api.user import api_router
+from app.api.user import api_router as user_router
 from app.config import project_settings
-from app.database import engine
-from app.models.base_class import Base
 
 
-def include_router(app):
-    app.include_router(api_router)
+app = FastAPI(**project_settings)
+app.include_router(user_router)
 
 
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-def start_application():
-    target_app = FastAPI(**project_settings)
-    create_db_and_tables()
-    include_router(target_app)
-    return target_app
-
-
-start_app = start_application()
-
-
-@start_app.get("/", tags=["test routes"])
+@app.get("/", tags=["test routes"])
 async def home():
     return {"msg": "Hello FastAPI🚀"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
