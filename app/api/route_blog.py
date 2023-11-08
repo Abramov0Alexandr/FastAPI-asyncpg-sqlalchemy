dd94f46e8bc6546e1091e8f68c1426193e7c09fe
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-11-09 00:40:33 +0300

    Написал маршруты и ORM запросы для получения списка объектов Blog и получение информации о конкретном объекте

--- app/api/route_blog.py
+++ app/api/route_blog.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, Depends
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 from app.database import get_async_session
-from app.orm.blog import create_new_blog
+from app.orm.blog import create_new_blog, retrieve_blog, list_blog
 from app.schemas import ShowBlog, BlogCreate
 
 
 api_router = APIRouter(prefix="/api", tags=["blog"])
 
 
-@api_router.post("/create_blog", response_model=ShowBlog,
+@api_router.post("/create_blog/", response_model=ShowBlog,
                  status_code=status.HTTP_201_CREATED,
                  description="Создание новой публикации для блога")
 async def create_blog(creation_blog_schema: BlogCreate, db: AsyncSession = Depends(get_async_session)):
@@ -17,5 +19,23 @@ async def create_blog(creation_blog_schema: BlogCreate, db: AsyncSession = Depen
     Маршрут для создания публикации в личном блоге.
     """
 
-    blog = await create_new_blog(creation_blog_schema=creation_blog_schema, async_db=db, author_id=1)
+    blog = await create_new_blog(blog_schema=creation_blog_schema, async_db=db, author_id=1)
     return blog
+
+
+@api_router.get("/detail_blog/", response_model=ShowBlog,
+                status_code=status.HTTP_200_OK, description='Получение информации о блоге')
+async def get_blog(blog_id: int, db: AsyncSession = Depends(get_async_session)):
+    blog = await retrieve_blog(blog_id=blog_id, async_db=db)
+
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Блога с ID: {blog_id} не существует")
+    return blog
+
+
+@api_router.get("/list_blog/", response_model=List[ShowBlog],
+                status_code=status.HTTP_200_OK, description="Получение списка созданных блогов")
+async def get_list_blog(db: AsyncSession = Depends(get_async_session)):
+    blog_list = await list_blog(async_db=db)
+    return blog_list
--- app/orm/blog.py
+++ app/orm/blog.py
@@ -1,18 +1,17 @@
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models import Blog
 from app.schemas import BlogCreate
 
 
-async def create_new_blog(creation_blog_schema: BlogCreate, async_db: AsyncSession, author_id: int):
-
+async def create_new_blog(blog_schema: BlogCreate, async_db: AsyncSession, author_id: int):
     """
     Асинхронная функция, представляющая собой ORM запрос.
     В ходе выполнения функции происходит создание нового блога.
     """
 
     query = insert(Blog).values(
-        **creation_blog_schema.model_dump(),
+        **blog_schema.model_dump(),
         author_id=author_id
     ).returning(Blog)
 
@@ -20,3 +19,25 @@ async def create_new_blog(creation_blog_schema: BlogCreate, async_db: AsyncSessi
     await async_db.commit()
     created_blog = result.scalar()
     return created_blog
+
+
+async def retrieve_blog(blog_id: int, async_db: AsyncSession):
+    """
+    Асинхронная функция, представляющая собой ORM запрос.
+    В ходе выполнения функции происходит поиск блога по переданному в запросе id.
+    """
+
+    query = select(Blog).filter(Blog.id == blog_id)
+    specific_blog = await async_db.execute(query)
+    return specific_blog.scalar()
+
+
+async def list_blog(async_db: AsyncSession):
+    """
+    Асинхронная функция, представляющая собой ORM запрос.
+    Функция позволяет получить список объектов модели Blog со статусом "is_active".
+    """
+
+    query = select(Blog).filter(Blog.is_active)
+    blog_list = await async_db.execute(query)
+    return blog_list.scalars()
