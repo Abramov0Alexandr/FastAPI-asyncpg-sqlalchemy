5b5b641e928abf25c86e6b1432d754724b1e94f7
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-11-10 00:24:24 +0300

    Написал маршрут и orm запрос для удаления объектов Blog, добавил проверку уникальности title

--- app/api/route_blog.py
+++ app/api/route_blog.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 from app.database import get_async_session
-from app.orm.blog import create_new_blog, retrieve_blog, list_blog, update_specific_blog
+from app.orm import blog as blog_orm
 from app.schemas import ShowBlog, BlogCreate, UpdateBlog
 
 api_router = APIRouter(prefix="/api", tags=["blog"])
@@ -17,14 +17,21 @@ async def create_blog(creation_blog_schema: BlogCreate, db: AsyncSession = Depen
     Маршрут для создания публикации в личном блоге.
     """
 
-    blog = await create_new_blog(blog_schema=creation_blog_schema, async_db=db, author_id=1)
+    check_blog_title = await blog_orm.get_blog_by_title(creation_blog_schema.title, async_db=db)
+
+    if check_blog_title:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Название для блога '{creation_blog_schema.title}' уже используется")
+
+    blog = await blog_orm.create_new_blog(blog_schema=creation_blog_schema, async_db=db, author_id=1)
     return blog
 
 
 @api_router.get("/detail_blog/{blog_id}/", response_model=ShowBlog,
                 status_code=status.HTTP_200_OK, description='Получение информации о блоге')
 async def get_blog(blog_id: int, db: AsyncSession = Depends(get_async_session)):
-    blog = await retrieve_blog(blog_id=blog_id, async_db=db)
+    blog = await blog_orm.retrieve_blog(blog_id=blog_id, async_db=db)
 
     if not blog:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
@@ -35,17 +42,34 @@ async def get_blog(blog_id: int, db: AsyncSession = Depends(get_async_session)):
 @api_router.get("/list_blog/", response_model=List[ShowBlog],
                 status_code=status.HTTP_200_OK, description="Получение списка созданных блогов")
 async def get_list_blog(db: AsyncSession = Depends(get_async_session)):
-    blog_list = await list_blog(async_db=db)
+    blog_list = await blog_orm.list_blog(async_db=db)
     return blog_list
 
 
 @api_router.put("/update_blog/{blog_id}/", response_model=ShowBlog,
                 status_code=status.HTTP_200_OK, description="Редактирование блога")
 async def update_blog(blog_id: int, schema: UpdateBlog, db: AsyncSession = Depends(get_async_session)):
-    blog = await update_specific_blog(blog_id=blog_id, blog_schema=schema, author_id=1, async_db=db)
+
+    check_blog_title = await blog_orm.get_blog_by_title(schema.title, async_db=db)
+
+    if check_blog_title:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Название для блога '{schema.title}' уже используется")
+
+    blog = await blog_orm.update_specific_blog(blog_id=blog_id, blog_schema=schema, author_id=1, async_db=db)
 
     if blog is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Блог с ID: {blog_id} не найден")
 
     return blog
+
+
+@api_router.delete("/delete_blog/{blog_id}/", status_code=status.HTTP_200_OK, description="Удаление блога")
+async def delete_blog(blog_id: int, db: AsyncSession = Depends(get_async_session)):
+    await blog_orm.destroy_blog(blog_id=blog_id, author_id=1, async_db=db)
+
+    return {
+        "status": status.HTTP_200_OK,
+        "detail": f"Блог с ID {blog_id} успешно удален"}
--- app/orm/blog.py
+++ app/orm/blog.py
@@ -1,7 +1,20 @@
-from sqlalchemy import insert, select, update
+from fastapi import HTTPException
+from sqlalchemy import insert, select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 from app.models import Blog
 from app.schemas import BlogCreate, UpdateBlog
+
+
+async def get_blog_by_title(blog_title: str, async_db: AsyncSession):
+    """
+    Асинхронная функция, представляющая собой ORM запрос.
+    При ее вызове происходит обращение к базе данных, результатом является объект модели Blog с указанным полем title.
+    """
+
+    query = select(Blog).where(Blog.title == blog_title)
+    result_blog = await async_db.execute(query)
+    return result_blog.scalar()
 
 
 async def create_new_blog(blog_schema: BlogCreate, async_db: AsyncSession, author_id: int):
@@ -44,7 +57,27 @@ async def list_blog(async_db: AsyncSession):
 
 
 async def update_specific_blog(blog_id: int, blog_schema: UpdateBlog, author_id: int, async_db: AsyncSession):
+    """
+    Асинхронная функция, представляющая собой ORM запрос.
+    Функция дл обновления объекта модели Blog.
+    """
+
     query = update(Blog).where(Blog.id == blog_id).values(blog_schema.model_dump(exclude_unset=True)).returning(Blog)
     result = await async_db.execute(query)
     await async_db.commit()
     return result.scalar()
+
+
+async def destroy_blog(blog_id: int, author_id: int, async_db: AsyncSession):
+    """
+    Асинхронная функция, представляющая собой ORM запрос.
+    Результатам выполнения функции является удаление объекта модели Blog.
+    """
+
+    query = delete(Blog).where(Blog.id == blog_id)
+    result = await async_db.execute(query.execution_options(synchronize_session="fetch"))
+
+    if result.rowcount == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Блог с ID {blog_id} не найден")
+
+    await async_db.commit()
