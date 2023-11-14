29d76860d8c491bb457be615120e60133f2d92a1
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-11-14 14:22:32 +0300

    Написал conftest для тестов, создал настройки для подключения к тестовой БД

---
+++ app/tests/conftest.py
@@ -0,0 +1,49 @@
+import asyncio
+from typing import AsyncGenerator
+import pytest
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from starlette.testclient import TestClient
+from app.config import test_db_settings
+from app.database import get_async_session
+from app.main import app
+from app.models.base_class import metadata, Base
+
+engine_test = create_async_engine(test_db_settings.DATABASE_URL)
+async_session = async_sessionmaker(engine_test, expire_on_commit=False, class_=AsyncSession)
+metadata.bind = engine_test
+
+
+async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_sessionmaker() as session:
+        yield session
+
+
+app.dependency_overrides[get_async_session] = override_get_async_session
+
+
+@pytest.fixture(autouse=True, scope='session')
+async def prepare_database():
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope='session')
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+sync_client = TestClient(app)
+
+
+@pytest.fixture(scope='session')
+async def async_client() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=app(), base_url='http://test') as client:
+        yield client
